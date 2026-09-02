3d067018fa44ad6e72dae82e8da243e69f2092ec
luizvdias-a11y <luiz_v_dias@estudante.sesisenai.org.br>
2026-09-02 10:00:15 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -7,5 +7,5 @@
 */
 window.BARBERFLOW_CONFIG = {
   SUPABASE_URL: "https://kjgcdsxlfyprrohtqczg.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_Eh8-5tOSD_I8eNlNYmjw7w_PGlhF7ry"
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqZ2Nkc3hsZnlwcnJvaHRxY3pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzMzc4OTcsImV4cCI6MjEwMzkxMzg5N30.DhSeUPZd5jRQtNT-j383LPTWOk03uaNusv3VUb6ZS1E"
 };
